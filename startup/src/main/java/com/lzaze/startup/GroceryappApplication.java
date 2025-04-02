19fac2f8ab8abc6229c0b18de1735171d8c839fb
Luthando Zaze <luthandozaze@gmail.com>
2025-04-02 21:06:09 +0200

    feat: initial push

---
+++ startup/src/main/java/com/lzaze/startup/GroceryappApplication.java
@@ -0,0 +1,13 @@
+package com.lzaze.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryappApplication.class, args);
+	}
+
+}
